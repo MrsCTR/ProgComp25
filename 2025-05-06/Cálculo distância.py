f9bdf6c566a7483b9c91e5d928eebd271b7e2fc9
CTR <camilatatiane.1994@gmail.com>
2025-05-06 17:50:50 -0300

    Cálculo distância.py

--- 2025-05-06/Cálculo distância.py
+++ 2025-05-06/Cálculo distância.py
@@ -1,13 +1,17 @@
+import sys
+
 velocidade = int (input (80))
 if velocidade <=0: 
-sys. exit ('informe velocidade positiva')
+   sys. exit ('informe velocidade positiva')
 
 Aceleração = int (input (40) m/s) 
 if Aceleração <=0: 
-sys. exit ('informe aceleração positiva')
+   sys. exit ('informe aceleração positiva')
 
 Tempo = int (input(10) m/s)
 if Tempo <=0:
-sys. exit ('informe tempo positivo')
+   sys. exit ('informe tempo positivo')
 
-print (f"A distância percorrida é: {distância percorrida} metros")
+Distância = v0 * t + (a * (t**2)) /2
+
+print (f'A distância percorrida é: {distância percorrida} metros')
