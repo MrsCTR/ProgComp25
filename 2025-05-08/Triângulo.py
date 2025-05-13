154457d2cd41676d8b123e6d2174e7085726a2a9
MrsCTR <camilatatiane.1994@gmail.com>
2025-05-13 17:50:08 -0300

    coordenadas.py

--- 2025-05-08/Triângulo.py
+++ 2025-05-08/Triângulo.py
@@ -8,3 +8,17 @@
    - Obstusângulo: Possui um ângulo interno obtuso (maior que 90).
    - Acutângulo: Possui todos os ângulos internos agudos (menores que 90).
 '''
+import sys
+
+# Solicitar os 3 ângulos do triângulo
+ÂnguloA = int(input('digite o ângulo A: '))
+ÂnguloB = int(input('digite o ângulo B: '))
+ÂnguloC = int(input('digite o ângulo C: '))
+
+# Verificando se os ângulos são positivos
+if (ÂnguloA <=0) or (ÂnguloB <=0) or (ÂnguloC <=0):
+    sys.exit('erro: um ou mais ângulos não são positivos.')
+
+# verificar se a soma dos ângulos é igual a 180
+if ÂnguloA + ÂnguloB + ÂnguloC !=180:
+    sys.exit
