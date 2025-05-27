d8cfce73cbc3ba3936e930d25fed0b4c060a0565
MrsCTR <camilatatiane.1994@gmail.com>
2025-05-27 17:48:10 -0300

    Soma.py

---
+++ 2025-05-27/Soma.py
@@ -0,0 +1,10 @@
+'''
+Programa para executar um produto de 2 números inteiros positivos utilizando apenas o operador soma (+)
+'''
+import sys
+soma = int(input('Digite 2 numeros positivos: '))
+
+sys.exit('erro: informe um numero valido')
+
+while soma <= 0
+Print(f'{soma} + {soma} = {resultado} + {resultado}')
