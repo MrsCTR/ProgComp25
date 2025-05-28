2eeb110219c616bf66a1f8c98cbeda02a2e3e387
MrsCTR <camilatatiane.1994@gmail.com>
2025-05-28 17:12:00 -0300

    Potenciação.py

--- 2025-05-27/Chave.py
+++ 2025-05-27/Chave.py
@@ -1,3 +1,12 @@
 '''
-Dada uma palavra chave , o programa só deve parar de pedir quando o usuário digitar a palavra chave.
+Dada uma palavra chave, o programa só deve parar de pedir quando
+o usuário digitar a palavra chave.
 '''
+
+strpalavrachave = '123Mudar'
+strpalavrainfo = ''
+
+while strpalavrainfo != strpalavrachave:
+    strpalavrainfo = input('digite a palavra chave: ')
+
+print('Você digitou a palavra chave...')
--- 2025-05-27/Potenciação.py
+++ 2025-05-27/Potenciação.py
@@ -1,3 +1,27 @@
 '''
 Programa para executar uma potenciação de 2 números inteiros utilizando apenas o operador produto (*)
 '''
+import sys
+
+try:
+    
+    intbase = int(input('informe a base... '))
+    intpotencia = int(input('Informe a potencia... '))
+except ValueError:
+    sys.exit('ERRO: Não foi informado um valor inteiro válido...')
+except Exception as e:
+    sys.exit(f'ERRO: {e}')
+else:
+    if intbase <= 0:
+        sys.exit('ERRO: Informe uma base positiva...')
+
+    if intpotencia <= 0:
+        sys.exit('ERRO: Informe uma potencia positiva...')
+
+    intpotenciacao = 1
+    Contador = 1
+    while Contador <= intpotencia:
+        intpotenciacao *= intbase
+        Contador += 1
+
+    print(f'{intbase} ** {intpotencia} = {Contador}')
