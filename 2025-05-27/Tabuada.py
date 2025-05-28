700f6ddbecde7fa7e8068bc9df325bc49e5c2e7e
MrsCTR <camilatatiane.1994@gmail.com>
2025-05-28 11:15:06 -0300

    Tabuada.py

--- 2025-05-27/Tabuada.py
+++ 2025-05-27/Tabuada.py
@@ -1,16 +1,20 @@
 # Solicita o número para calcular
 
 import sys
+
 try:
-    multiplicador = int(input('Digite o multiplicador da tabuada: '))
+   Multiplicador = int(input('Informe o Multiplicador: '))
 except ValueError:
-    sys.exit('erro:informe um valor inteiro.')
-else:
-    if multiplicador <= 1
+   sys.exit('ERRO: Informe um valor inteiro...')
+except Exception as e:
+   sys.exit(f'ERRO {e}')
+else:  
+   if Multiplicador <= 0:
+      sys.exit('ERRO: Informe um valor inteiro positivo...')
 
-multiplicando = 1
-while multiplicando <= 10:
-    print(f'{multiplicador} x {multiplicando} = {multiplicador} * {multiplicando}')
-    multiplicando += 1
+   Multiplicando = 1
+   while Multiplicando <= 10:
+      print(f'{Multiplicador} x  {Multiplicando} = {Multiplicador * Multiplicando}')
+      Multiplicando += 1
 
-print('fim da tabuada...')
+   print('FIM DA TABUADA...')
