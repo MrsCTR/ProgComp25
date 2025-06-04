0ef22370a625e87604d86dc0591036b6e3f95336
MrsCTR <camilatatiane.1994@gmail.com>
2025-06-04 17:52:44 -0300

    MDC1.py

--- 2025-06-04/MDC1.py
+++ 2025-06-04/MDC1.py
@@ -2,3 +2,26 @@
    Dados dois números inteiros positivos, determinar o Máximo Divisor 
    Comum (MDC) entre eles usando o Algoritmo de Euclides.
 '''
+
+import sys
+
+try:
+   
+   MDC1 = int(input('informe um MDC1:'))
+   MDC2 = int(input('informe um MDC2:'))
+
+   if MDC1 <= 0 or MDC2 <= 0:
+       sys.exit('ERRO: Informe um número inteiro positivo...')
+
+   for _ in range(MDC2):
+      resto = MDC1 % MDC2
+      MDC1 = MDC2
+      MDC2 = resto
+      if MDC2 == 0:
+         break
+      print(f'O MDC é {MDC1}')
+
+except ValueError:
+   sys.exit('ERRO: Não foi informado um MDC válido...')
+except Exception as e:
+   sys.exit(f'ERRO: {e}')
