2a4cabe55dfecf7967fefd177e7ea05b96a62a93
MrsCTR <camilatatiane.1994@gmail.com>
2025-06-10 17:50:33 -0300

    Exemplo3.py

---
+++ 2025-06-10 String/Exemplo3.py
@@ -0,0 +1,13 @@
+'''
+   Programa para verificar quantas vogais existem em uma string informada pelo usuário.Add commentMore actions
+'''
+strTexto = input('Digite um texto: ').upper()
+strVogais = 'AEIOUÂÊÎÔÛÃÕÁÉÍÓÚ'
+Vogais = 0
+
+
+for strLetra in strTexto:
+    if strLetra in strVogais:
+        Vogais += 1
+
+        print()
