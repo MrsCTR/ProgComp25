157a7b14ec3bf7618b1b4888d9ab57d3c2d62361
MrsCTR <camilatatiane.1994@gmail.com>
2025-07-01 17:28:20 -0300

    Lista1.py

--- 2025-06-26/Lista1.py
+++ 2025-06-26/Lista1.py
@@ -23,7 +23,6 @@
 '''
 import random
 
-
 lstNomes = ['Scooby-Doo'       , 'Fred Flintstone', 'Zé Colmeia' , 'Dom Pixote'     , 
             'Muttley'          , 'Bionicão'       , 'Tutubarão'  , 'Capitão Caverna', 
             'Formiga Atômica'  , 'Jonny Quest'    , 'Space Ghost', 'Manda-Chuva'    , 
@@ -32,50 +31,55 @@ lstNomes = ['Scooby-Doo'       , 'Fred Flintstone', 'Zé Colmeia' , 'Dom Pixote'
 
 # ----------------------------------------------------------------------
 # Questão 01
-listaNotas_1 = list()
-listaNotas_2 = list()
+lstNotas_1 = list()
+lstNotas_2 = list()
 
 for _ in lstNomes:
-    listaNotas_1.append(random.randint)
-    listaNotas_2.append(random.randint)
+    lstNotas_1.append(random.randint(0, 100))
+    lstNotas_2.append(random.randint(0, 100))
 
 # ----------------------------------------------------------------------
 # Questão 02
+lstBoletins = list()
+
 for i in range(len(lstNomes)):
-    listaboletins =
+    lstBoletins.append([lstNomes[i], lstNotas_1[i], lstNotas_2[i]])
+
 
 # ----------------------------------------------------------------------
 # Questão 03
-for boletim in listaboletins:
+for boletim in lstBoletins:
     media = int(round((boletim[1]*2 + boletim[2]*3) / 5, 0))
-
-    if media >= 0:
+    
+    if media >= 60:
         situacao = 'Aprovado'
     elif media >= 20:
         situacao = 'Prova Final'
     else:
         situacao = 'Reprovado'
+    
+    #boletim.append(media)
+    #boletim.append(situacao)
 
-
-boletim.extend([media, situacao])
+    boletim.extend([media, situacao])
 
 
 # ----------------------------------------------------------------------
 # Questão 04
 # TODO: Fazer na sala de aula no dia 01/07/2026
 # TODO: Pesquisar a função SORT() usando funções LAMBDA
-listaboletins.sort(key=lambda aluno: aluno[3], reverse=True)
+lstBoletins.sort(key=lambda aluno: aluno[3], reverse=True)
 
-for aluno in listaboletins:
-    print(f'{aluno[0]:<20} ... {aluno[2]:>3} ... {aluno[3]:>3} ... {aluno[4]}')
+for aluno in lstBoletins:
+    print(f'{aluno[0]:<20} ... {aluno[1]:>3} ... {aluno[2]:>3} ... {aluno[3]:>3} ... {aluno[4]}')
 
 
 # ----------------------------------------------------------------------
 # Questão 04
 # TODO: Fazer na sala de aula no dia 01/07/2026
 # TODO: Pesquisar a função FILTER() usando funções LAMBDA
-lstAprovados = filter(lambda aluno:aluno[4] == 'Aprovado', listaboletins)
+lstAprovados = list( filter(lambda aluno:aluno[4] == 'Aprovado', lstBoletins))
 
-print('_'*100)
+print('-'*100)
 for aluno in lstAprovados:
-    print(f'{aluno[0]:<20} ... {aluno[2]:>3} ... {aluno[3]:>3} ... {aluno[4]}')
+    print(f'{aluno[0]:<20} ... {aluno[1]:>3} ... {aluno[2]:>3} ... {aluno[3]:>3} ... {aluno[4]}')
