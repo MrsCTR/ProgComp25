856b138c854a7436cbbc4aca7001606864a99bf5
MrsCTR <camilatatiane.1994@gmail.com>
2025-07-09 17:39:06 -0300

    leiturasemheader.py

---
+++ 2025-07-09/leiturasemheader.py
@@ -0,0 +1,32 @@
+import os, sys
+
+strDir = os.path.dirname(__file__)
+
+try:
+    arqLeitura = open(f'{strDir}//times.csv', 'r', encoding= 'utf-8')
+
+except FileNotFoundError:
+    sys.exit('Erro: Arquivo não encontrado!')
+except Exception as erro:
+    sys.exit(f'Erro:{erro}!')
+
+else:
+
+    lstTimes = list()
+
+    while True:
+       strLinha = arqLeitura.readline().strip()
+       if not strLinha: break
+       lstAux = [int(i) if i.isdigit() else i for i in strLinha.split(';')]
+       lstTimes.append(lstAux)
+    arqLeitura.close()
+
+for time in lstTimes:
+    time.insert(4, time[1]*3 + time[2])
+    time.append(time[5] - time[6])
+
+
+lstTimes.sort(key=lambda time: (time[4], time[1], time[7], time[5]), reverse=True)
+
+for time in lstTimes:
+    print(time)
