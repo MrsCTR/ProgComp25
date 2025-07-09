639eda16412c7f3110eb67e7013cb9a8b86ebf9e
MrsCTR <camilatatiane.1994@gmail.com>
2025-07-09 17:28:23 -0300

    leitura.py

--- 2025-07-09/leitura.py
+++ 2025-07-09/leitura.py
@@ -20,6 +20,11 @@ else:
        lstAux = [int(i) if i.isdigit() else i for i in strLinha.split(';')]
        lstTimes.append(lstAux)
     arqLeitura.close()
+for time in lstTimes:
+    time.insert(4, time[1]*3 + time[2])
+    time.append(time[5] - time[6])
 
+lstTimes.sort(key=lambda time: (time[4], time[1], time[7], time[5]), reverse=True)
 
-print(lstTimes)
+for time in lstTimes:
+    print(time)
