d12e53fec4c45d40c92b463a832a985e69b762a1
MrsCTR <camilatatiane.1994@gmail.com>
2025-07-22 17:41:03 -0300

    Dicionarios.py

---
+++ 2025-07-22/Dicionarios.py
@@ -0,0 +1,36 @@
+import os, sys, json
+
+strDirApp = os.path.dirname(__file__)
+
+strNomeArq = f'{strDirApp}//cartola_fc_2024.json'
+
+try:
+    arqInput = open(strNomeArq, 'r', encoding='utf-8')
+    strDados = arqInput.read()
+    dictCartola = json.loads(strDados)
+    arqInput.close()
+
+except FileNotFoundError:
+    sys.exit('Erro: Arquivo não existe...')
+except json.JSONDecodeError:
+    sys.exit('Erro: O conteúdo do arquivo não está no formato correto')
+except Exception as erro:
+    sys.exit(f'Erro: {erro}...')
+else:
+    lstChaves = list(dictCartola.keys()) # ['clubes', 'posicoes', 'status', 'atletas']
+    '''
+    dictCartola = { 'clubes': {...}, 'posicoes': {...}, 'status': {...}, 'atletas': [...]
+
+    dictCartola['clubes']    -> dicionário (k:v)
+    dictCartola['posicoes']  -> dicionário (k:v)
+    dictCartola['status']    -> dicionário (k:v)
+    dictCartola['atletas']   -> lista (indice -> dicionário (k:v)
+    '''
+
+# Informando o nome do clube    
+strClube = input('Informe o nome do clube: ').strip().lower()
+
+# Listando os atletas do clube informado
+
+
+   
