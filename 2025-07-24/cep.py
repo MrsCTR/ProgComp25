aa5838d1cce2280c0ef73ad63969a3d1d51a665a
MrsCTR <camilatatiane.1994@gmail.com>
2025-07-24 17:13:27 -0300

    cep.py

--- 2025-07-24/cep.py
+++ 2025-07-24/cep.py
@@ -1,7 +1,7 @@
 import requests, sys
 
 try:
-    reqHTTP = requests.get('https://api.cartolafc.globo.com/atletas/mercado')
+    reqHTTP = requests.get('https://viacep.com.br/ws/59575000/json')
 
 except Exception as erro:
     sys.exit(f'Erro: {erro}')
